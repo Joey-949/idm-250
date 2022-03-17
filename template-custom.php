efce84213112d8f86567f27bad44623654728d22
Joey-949 <71980853+Joey-949@users.noreply.github.com>
2022-03-17 14:44:29 -0400

    Add files via upload

---
+++ template-custom.php
@@ -0,0 +1,31 @@
+<?php
+/* Template Name: Custom Template */
+?>
+
+<?php get_header(); ?>
+<?php
+get_template_part(
+    'components/heros/home-hero',
+    null,
+    [
+        'heading' => 'Example Template Page',
+        'body' => ''
+    ]
+);
+?>
+
+
+<?php while (have_posts()) : the_post(); ?>
+<div class="" style="max-width: 800px; margin: 0 auto;">
+  <h1 class=""><?php the_title();?>
+  </h1>
+
+  <div class="">
+    <!-- start content -->
+    <?php the_content(); ?>
+    <!-- end content -->
+  </div>
+</div>
+<?php endwhile; ?>
+
+<?php get_footer();
